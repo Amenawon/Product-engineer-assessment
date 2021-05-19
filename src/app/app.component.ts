584c41a9d6d92190e90fd69c4e26477fd751ed55
Amenawon <menaesezobor@gmail.com>
2021-05-19 21:08:28 +0100

    add methods to getdistance in km

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
+import { Constants } from "./core/model/Constants";
 import { Customer } from "./core/model/Customer";
 import { CustomerService } from "./core/service/customer.service";
 
@@ -12,16 +13,19 @@ export class AppComponent implements OnInit {
   title = "Intercom-Interview-Assessment";
   customers: Customer[];
   sortedCustomers: Customer[];
-
+  invitedCustomers: Customer[];
+  earthMeanRadiusKm = 6371;
+  centerLatitude = 53.339428;
+  centerLongitude = -6.257664;
   constructor(private customerService: CustomerService) {}
   ngOnInit(): void {
     this.processCustomerData();
   }
   processCustomerData() {
     try {
-    this.getCustomers();
+      this.getCustomers();
     } catch (error) {
-     return console.log(error,'unable to load customer records'); 
+      return console.log(error, "unable to load customer records");
     }
   }
 
@@ -30,8 +34,6 @@ export class AppComponent implements OnInit {
       (response) => {
         this.customers = response;
         this.sortedCustomers = this.sortCustomersByUserId();
-        console.log(this.sortedCustomers)
-
       },
       (err: HttpErrorResponse) => {
         console.log(err, "http error response");
@@ -39,14 +41,45 @@ export class AppComponent implements OnInit {
     );
   }
 
-  
   sortCustomersByUserId(): Customer[] {
-    return this.customers.sort(
-      (a: Customer, b: Customer) => {
-        if (a.user_id < b.user_id) return -1;
-        if (a.user_id > b.user_id) return 1;
-        return 0;
-      }
+    return this.customers.sort((a: Customer, b: Customer) => {
+      if (a.user_id < b.user_id) return -1;
+      if (a.user_id > b.user_id) return 1;
+      return 0;
+    });
+  }
+
+
+  getDistanceInKm(
+    userLatitude,
+    userLongitude,
+    cityLatitude,
+    cityLongitude,
+    radius
+  ): number {
+    const phi1 = (userLatitude * Math.PI) / 180;
+    const lambda1 = (userLongitude * Math.PI) / 180;
+    const phi2 = (cityLatitude * Math.PI) / 180;
+    const lambda2 = (cityLongitude * Math.PI) / 180;
+
+    const deltaLambda = Math.abs(
+      lambda1 > lambda2 ? lambda1 - lambda2 : lambda2 - lambda1
     );
+
+     const angleRad = Math.atan2(
+      Math.sqrt(
+        Math.pow(Math.cos(phi2) * Math.sin(deltaLambda), 2) +
+          Math.pow(
+            Math.cos(phi1) * Math.sin(phi2) -
+              Math.sin(phi1) * Math.cos(phi2) * Math.cos(deltaLambda),
+            2
+          )
+      ),
+
+      Math.sin(phi1) * Math.sin(phi2) +
+        Math.cos(phi1) * Math.cos(phi2) * Math.cos(deltaLambda)
+    );
+
+    return angleRad * radius;
   }
 }
